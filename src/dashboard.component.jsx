512df3f442d67f556328a8f3927cc72ef2cd8703
Diana Hamzea <D.Hamzea@asctechnologies.com>
2021-05-05 19:42:08 +0300

    initial commit

---
+++ src/dashboard.component.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './dashboard.styles.scss';
+const Dashboard = () =>(
+    <div className='dashboard'>
+        <div class='dashboard-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>TASKS</h1>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>fun time</h1>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>myself</h1>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>TASKS2</h1>
+                </div>
+            </div>
+        </div>
+
+    </div>
+);
+
+export default Dashboard;
