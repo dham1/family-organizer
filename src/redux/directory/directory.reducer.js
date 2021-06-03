aff22f334bc611b92443a18507e2768d0cc86e98
Diana Hamzea <D.Hamzea@asctechnologies.com>
2021-06-03 17:20:32 +0300

    get data from db, added spinner

--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -4,33 +4,33 @@ const INITIAL_STATE = {
       title: 'Cactus & Succulents',
       imageUrl: 'https://i.ibb.co/8501Hz9/suculenta-Cal.jpg',
       id: 1,
-      linkUrl: 'shop/succulents'
+      linkUrl: 'shop/cactus%20&%20succulents'
     },
     {
       title: 'Floral Arrangements',
       imageUrl: 'https://i.ibb.co/ThSMyz6/IMG-0402.jpg',
       id: 2,
-      linkUrl: 'shop/arrangements'
+      linkUrl: 'shop/floral%20arrangements'
     },
     {
       title: 'Flower Plants',
       imageUrl: 'https://i.ibb.co/tXzC0KD/lalele-Roz.jpg',
       id: 3,
-      linkUrl: 'shop/flowers'
+      linkUrl: 'shop/flower%20plants'
     },
     {
       title: 'Green Plants',
       imageUrl: 'https://i.ibb.co/Bf2sVRC/40736-CB3-D0-ED-4-D33-8-C15-736-DC57-ACE4-D.jpg',
       size: 'large',
       id: 4,
-      linkUrl: 'shop/greenPlants'
+      linkUrl: 'shop/green%20plants'
     },
     {
-      title: 'Purifying Plants',
+      title: 'purifying%20plants',
       imageUrl: 'https://i.ibb.co/bzWJBd6/0473695-B-C797-4-C27-9-FBD-451-B91928-B49.jpg',
       size: 'large',
       id: 5,
-      linkUrl: 'shop/purifyingPlants'
+      linkUrl: 'shop/purifying%20plants'
     }
   ]
 };
